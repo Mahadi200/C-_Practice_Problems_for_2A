c5bcf01ad4d412fe01ce1cad5f427cc7faa9a742
Mahadi200 <shurov895@gmail.com>
2025-01-16 23:54:44 +0600

    prb 3

--- problem2.c
+++ problem2.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int A[2][2], B[2][2], result[2][2];
     int i, j, k;
 
     // Input Matrix A
     printf("Enter elements of Matrix A (2x2): \n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             scanf("%d", &A[i][j]);
         }
     }
 
     // Input Matrix B
     printf("Enter elements of Matrix B (2x2): \n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             scanf("%d", &B[i][j]);
         }
     }
 
     // Initialize Resultant Matrix
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             result[i][j] = 0;
         }
     }
 
     // Multiply Matrices A and B
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
-            for (k = 0; k < 2; k++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            for (k = 0; k < 2; k++)
+            {
                 result[i][j] += A[i][k] * B[k][j];
             }
         }
@@ -38,8 +48,10 @@ int main() {
 
     // Print Resultant Matrix
     printf("Resultant Matrix:\n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 2; j++) {
+    for (i = 0; i < 2; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
             printf("%d ", result[i][j]);
         }
         printf("\n");
